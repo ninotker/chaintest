a364c78b8368e19e8bb9c887637f5b750509266e
ninotker <luka.milosa@gmail.com>
2023-07-01 11:41:36 +0200

    other port

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const server = new ProxyChain.Server({
     port: 4323,
     verbose: true,
     prepareRequestFunction: ({ request, username, password, hostname, port, isHttp, connectionId }) => {
+        request.headers["lol"] = 1
         return {
             requestAuthentication: false,
-            upstreamProxyUrl: `http://Supq:ISJB@localhost:4026`,
+            upstreamProxyUrl: `http://Supq:ISJB@51.178.43.147:4026`,
         };
     },
 });
